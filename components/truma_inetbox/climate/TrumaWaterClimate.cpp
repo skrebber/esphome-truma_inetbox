847ba0d328e3f8157b00c47bd07ddc562a5b3d55
Sönke <s-krebber@imail.de>
2025-11-28 14:32:59 +0100

    test3
    test3

--- components/truma_inetbox/climate/TrumaWaterClimate.cpp
+++ components/truma_inetbox/climate/TrumaWaterClimate.cpp
@@ -45,7 +45,7 @@ climate::ClimateTraits TrumaWaterClimate::traits() {
   // The capabilities of the climate device
   auto traits = climate::ClimateTraits();
   //traits.set_supports_current_temperature(true);
-  traits.add_feature_flags(CLIMATE_SUPPORTS_CURRENT_TEMPERATURE)
+  traits.add_feature_flags(esphome::climate::CLIMATE_SUPPORTS_CURRENT_TEMPERATURE)
   traits.set_supported_modes(this->supported_modes_);
   traits.set_visual_min_temperature(40);
   traits.set_visual_max_temperature(80);
